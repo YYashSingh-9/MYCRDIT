668ee027cb70976e6d38c2181c0dacc23bdf2a48
YYashSingh-9 <yyashsngh@gmail.com>
2024-05-12 08:45:42 -0700

    more

--- src/Components/TopLevel/Footer.jsx
+++ src/Components/TopLevel/Footer.jsx
@@ -1,5 +1,10 @@
 import { Box, Grid } from "@mui/material";
 import classes from "./Footer.module.css";
+import playstoreImg from "../../assets/play.png";
+import appleStoreImg from "../../assets/apple.png";
+import XIcon from "@mui/icons-material/X";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 const Footer = () => {
   return (
@@ -9,14 +14,22 @@ const Footer = () => {
         direction="column"
         justifyContent="center"
         alignItems="center"
+        className={classes.footerContainer}
       >
-        <Grid item xs={12} md={12} lg={12}>
-          <Box>
+        <Grid item xs={12} md={12} lg={12} className={classes.footerTop}>
+          <Box className={classes.storelinks}>
             <h3>
               Download the MYCRDIT app
               <br />
               for better pay later service
             </h3>
+            <img src={playstoreImg} />
+            <img src={appleStoreImg} />
+          </Box>
+          <Box className={classes.socialLinksPart}>
+            <XIcon />
+            <InstagramIcon />
+            <FacebookIcon />
           </Box>
         </Grid>
       </Grid>
