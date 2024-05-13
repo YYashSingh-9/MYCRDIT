98b9b59d3c5222d95858c99f8ae0493b624b7128
YYashSingh-9 <yyashsngh@gmail.com>
2024-05-13 05:17:29 -0700

    more

--- src/Components/TopLevel/Footer.jsx
+++ src/Components/TopLevel/Footer.jsx
@@ -5,6 +5,7 @@ import appleStoreImg from "../../assets/apple.png";
 import XIcon from "@mui/icons-material/X";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -27,9 +28,29 @@ const Footer = () => {
             <img src={appleStoreImg} />
           </Box>
           <Box className={classes.socialLinksPart}>
-            <XIcon className={classes.icon} />
-            <InstagramIcon className={classes.icon} />
-            <FacebookIcon className={classes.icon} />
+            <Link to={"https://twitter.com/Yash__9999"}>
+              <XIcon className={classes.icon} />
+            </Link>
+            <Link to={"https://www.instagram.com/yasshwardhan_siingh/"}>
+              <InstagramIcon className={classes.icon} />
+            </Link>
+            <Link
+              to={"https://www.linkedin.com/in/yashwardhan-singh-083064291/"}
+            >
+              <FacebookIcon className={classes.icon} />
+            </Link>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={12} lg={12} className={classes.footerBottom}>
+          <Box className={classes.footerText}>
+            <hr />
+            <h4>This is prototype v1.0, developed by Yasshwardhan Siingh</h4>
+            <h5>
+              <span style={{ textDecoration: "underline" }}>
+                Copyright 2024
+              </span>
+              , All Rights reserved
+            </h5>
           </Box>
         </Grid>
       </Grid>
