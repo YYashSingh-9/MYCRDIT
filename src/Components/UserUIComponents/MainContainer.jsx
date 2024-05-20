f21476db2c540581571f3b2cb9c0b987ac235504
YYashSingh-9 <yyashsngh@gmail.com>
2024-05-20 06:20:23 -0700

    UserUI Done

--- src/Components/UserUIComponents/MainContainer.jsx
+++ src/Components/UserUIComponents/MainContainer.jsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, colors } from "@mui/material";
 import classes from "./MainContainer.module.css";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
 const MainContainer = () => {
@@ -11,20 +11,35 @@ const MainContainer = () => {
         justifyContent="center"
         className={classes.main}
       >
-        <Grid item lg={12} md={12} sm={12} xs={12}>
+        <Grid item lg={12} md={12} sm={12} xs={12} className={classes.outerBox}>
           <Box className={classes.innerBox}>
             <Box className={classes.scoreCircle}>
               <h1>1</h1>
             </Box>
-            <h3>Your current MYCrdit Score</h3>
+            <h3>
+              Your current <span style={{ color: "#1db954" }}> MYCrdit</span>{" "}
+              Score
+            </h3>
           </Box>
         </Grid>
-        <Grid item lg={12} md={12} sm={12} xs={12}>
+        <Grid
+          item
+          lg={12}
+          md={12}
+          sm={12}
+          xs={12}
+          className={classes.gridOuter}
+          marginTop={4}
+          marginBottom={4}
+        >
           <Box className={classes.tipBox}>
-            <TipsAndUpdatesIcon />
-            <h3>Tips:</h3>
+            <TipsAndUpdatesIcon className={classes.tipIcon} />
+            <h3>Tip :</h3>
           </Box>
-          <h4>Clear your debt bills on time to increase your MYCrdit Score</h4>
+          <h4>
+            Clear your debt bills on time to increase your MYCrdit Score{" "}
+            {`(MCS)`}
+          </h4>
         </Grid>
       </Grid>
     </>
