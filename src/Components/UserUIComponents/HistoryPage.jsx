1909209d634ef66d0de0ccc85b52990b57769ef7
YYashSingh-9 <yyashsngh@gmail.com>
2024-05-22 12:36:36 -0700

    more

--- src/Components/UserUIComponents/HistoryPage.jsx
+++ src/Components/UserUIComponents/HistoryPage.jsx
@@ -1,10 +1,32 @@
 import classes from "./HistoryPage.module.css";
 import BasicCoverDiv from "../AdditionalComponents/BasicCoverDiv";
-import { Grid } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import { useSelector } from "react-redux";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+
+const ListItem = (props) => {
+  return (
+    <>
+      <Box className={classes.liBox}>
+        <Box className={classes.info_left}>
+          <h3>{props.title}</h3>
+          <h4>{props.date}</h4>
+        </Box>
+        <Box className={classes.info_right}>
+          <h4>{`${props.amt}/-`}</h4>
+          <button className={classes.clearIndicatorBtn}>
+            <DoneAllIcon className={classes.cleartick} />
+            {props.btnTitle}
+          </button>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 const HistoryPage = () => {
   const accType = useSelector((state) => state.sliceOne.accountType);
+
   return (
     <>
       <BasicCoverDiv>
@@ -17,8 +39,46 @@ const HistoryPage = () => {
           md={12}
           sm={12}
           xs={12}
+          marginBottom={3}
           className={classes.detailsBox}
-        ></Grid>
+        >
+          <ListItem
+            title={"Mishra ji,Sunflower oil"}
+            date={"14-06-24"}
+            amt={250}
+            btnTitle={"Cleared"}
+          />
+          <ListItem
+            title={"Mishra ji,Sunflower oil"}
+            date={"14-06-24"}
+            amt={250}
+            btnTitle={"Cleared"}
+          />{" "}
+          <ListItem
+            title={"Mishra ji,Sunflower oil"}
+            date={"14-06-24"}
+            amt={250}
+            btnTitle={"Cleared"}
+          />
+          <ListItem
+            title={"Mishra ji,Sunflower oil"}
+            date={"14-06-24"}
+            amt={250}
+            btnTitle={"Cleared"}
+          />{" "}
+          <ListItem
+            title={"Mishra ji,Sunflower oil"}
+            date={"14-06-24"}
+            amt={250}
+            btnTitle={"Cleared"}
+          />{" "}
+          <ListItem
+            title={"Mishra ji,Sunflower oil"}
+            date={"14-06-24"}
+            amt={250}
+            btnTitle={"Cleared"}
+          />
+        </Grid>
       </BasicCoverDiv>
     </>
   );
