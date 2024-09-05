f5b8acf3af85394a00704a33f33a639ab3064d1f
YYashSingh-9 <yyashsngh@gmail.com>
2024-09-05 05:01:00 -0700

    UI Updates

--- src/Components/AdditionalComponents/FiltersDiv.jsx
+++ src/Components/AdditionalComponents/FiltersDiv.jsx
@@ -2,21 +2,24 @@ import { Container, Box, Typography } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import classes from "./FiltersDiv.module.css";
 
+const styles = {
+  heading: {
+    fontWeight: 800,
+    color: "white",
+    fontFamily: "Manrope",
+    paddingTop: 1,
+    "@media only screen and (min-width: 280px) and (max-width: 576px)": {
+      display: "none",
+    },
+  },
+};
+
 const FilterDiv = () => {
   return (
     <>
       <Box>
         <Box sx={{ width: "100%", textAlign: "center" }}>
-          <Typography
-            variant="h5"
-            margin={"auto"}
-            sx={{
-              fontWeight: 800,
-              color: "white",
-              fontFamily: "Manrope",
-              paddingTop: 1,
-            }}
-          >
+          <Typography variant="h5" margin={"auto"} sx={styles.heading}>
             FILTER
           </Typography>
         </Box>
