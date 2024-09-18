3a2c308c0085cf2efc25dda4dd4beb2a257edc2f
YYashSingh-9 <yyashsngh@gmail.com>
2024-09-18 08:52:03 -0700

    Adding slider Animation

---
+++ src/Components/AdditionalComponents/InitialSlider.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import classes from "InitialSlider.module.css";
+
+const InitialSlider = () => {
+  return (
+    <>
+      <Box className={classes.mainSlider}></Box>
+    </>
+  );
+};
+
+export default InitialSlider;
--- src/Components/TopLevel/AccountPage.jsx
+++ src/Components/TopLevel/AccountPage.jsx
@@ -9,15 +9,7 @@ const AccountPage = () => {
 
   return (
     <>
-      <BasicCoverDiv>
-        <Grid item lg={12} md={12} xs={12} sm={12} className={classes.header}>
-          <h1>
-            Account of{" "}
-            <span style={{ color: "#1DB954", fontFamily: "poppins" }}>
-              Alokik
-            </span>
-          </h1>
-        </Grid>
+      <BasicCoverDiv heading="Account of" heading_highlight=" Alokik">
         <Grid item lg={12} md={12} xs={12} sm={12} className={classes.infoPart}>
           <AccountInfoPart accountType={accType} />
         </Grid>
