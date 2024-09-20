f89a832854fa7802620d0b610bc31a00ec62467f
YYashSingh-9 <yyashsngh@gmail.com>
2024-09-20 08:58:30 -0700

    adding models

---
+++ Backend/Models/customerModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const customerSchema = new mongoose.Schema({
+  customerName: {
+    type: String,
+    required: true,
+    minlength: [5, "Minimum 5 characters required. "],
+    maxlength: [20, "Maximum 20 characters allowed. "],
+  },
+  contactNumber: {
+    type: Number,
+    unique: true,
+    minlength: [10, "10 character number is required. "],
+    required: true,
+  },
+  password: {
+    type: String,
+    required: [true, "You must have a password. "],
+    minlength: [8, "Password must be 8 characters long. "],
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please provide confirm password. "],
+    minlength: [8, "Password must be 8 character long. "],
+    validate: {},
+  },
+});
