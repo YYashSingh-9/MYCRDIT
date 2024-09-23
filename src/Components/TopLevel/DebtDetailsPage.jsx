9418d2f7c7f8090710fd0a56cadd341442814a65
YYashSingh-9 <yyashsngh@gmail.com>
2024-09-23 05:18:28 -0700

    more

--- src/Components/TopLevel/DebtDetailsPage.jsx
+++ src/Components/TopLevel/DebtDetailsPage.jsx
@@ -3,6 +3,7 @@ import classes from "./DebtDetails.module.css";
 import { Link } from "react-router-dom";
 import InitialSlider from "../AdditionalComponents/InitialSlider";
 import DetailedNote from "../AdditionalComponents/DetailDebtNote";
+import EditIcon from "@mui/icons-material/Edit";
 
 const DebtDetailsPage = () => {
   return (
@@ -21,7 +22,12 @@ const DebtDetailsPage = () => {
           </header>
         </Grid>
         <Grid item lg={12} sm={12} md={12} xs={12} className={classes.addBtn}>
-          <button>Add to this a/c</button>
+          <Link>
+            <button>
+              <EditIcon className={classes.iccn} />
+              Add to this a/c
+            </button>
+          </Link>
         </Grid>
         <Grid
           item
