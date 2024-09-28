578e5ae7d27da22cd872a562300224fd9674f92a
YYashSingh-9 <yyashsngh@gmail.com>
2024-09-28 07:18:33 -0700

    adding validations

--- Backend/Models/proprietorModel.js
+++ Backend/Models/proprietorModel.js
@@ -45,5 +45,8 @@ const proprietorSchema = mongoose.Schema({
     required: [true, "Please enter password"],
     minLength: [5, "Password must be at lease 5 characters long"],
     maxLength: [16, "Password must not exceed 16 characters."],
+    validate: function (el) {
+      return el === this.password;
+    },
   },
 });
