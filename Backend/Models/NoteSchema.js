9cf329db5163abfd8f41ff37265750f3a24fdac5
YYashSingh-9 <yyashsngh@gmail.com>
2024-10-06 08:53:19 -0700

    debt Note model done

--- Backend/Models/NoteSchema.js
+++ Backend/Models/NoteSchema.js
@@ -8,22 +8,38 @@ const noteSchema = mongoose.Schema({
   },
   customerName: {
     type: String,
-    required: [true, "Provde customer name"],
+    required: [true, "Provde customer name."],
     minLength: [5, "Minimum 5 characters are required."],
   },
   customerNumber: {
     type: Number,
-    required: [true, "Please provide customer number"],
-    minLength: [10, "10 characters are required"],
-    maxLength: [10, "10 characters are required"],
+    required: [true, "Please provide customer number."],
+    minLength: [10, "10 characters are required."],
+    maxLength: [10, "10 characters are ."],
   },
   productBrand: {
     type: String,
-    required: [true, "Provide product brand name"],
-    minLength: [5, "Minimum 5 characters are required"],
-    maxLength: [30, "Reached 30 characters limit"],
+    required: [true, "Provide product brand name."],
+    minLength: [5, "Minimum 5 characters are required."],
+    maxLength: [30, "Reached 30 characters limit."],
   },
   productName: {
     type: String,
+    required: [true, "Please provide product name."],
+    minLength: [5, "Minimum 5 characters required."],
+    maxLength: [30, "Reached 30 characters limit."],
+  },
+  amount: {
+    type: Number,
+    required: [true, "Enter debt amount."],
+    minLength: [1, "Minimum 1 digit amount is required."],
+  },
+  date: {
+    type: Date,
+    required: [true, "Please provide current date."],
   },
 });
+
+const DebtNote = mongoose.model("DebtNote", noteSchema);
+
+module.exports = DebtNote;
