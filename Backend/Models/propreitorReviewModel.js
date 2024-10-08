c90b651584fab6072bb239c1230228998f86e62e
YYashSingh-9 <yyashsngh@gmail.com>
2024-10-08 06:08:31 -0700

    defining review schema

--- Backend/Models/propreitorReviewModel.js
+++ Backend/Models/propreitorReviewModel.js
@@ -5,4 +5,16 @@ const proprietorReviewSchema = mongoose.Schema({
     type: String,
     required: [true, "Name required."],
   },
+  brandName: {
+    type: String,
+    required: [true, "Provide Brand name"],
+    minLength: [5, "Minimum 5 characters required."],
+    maxLength: [30, "Maximum 30 characters reached."],
+  },
+  pincode: {
+    type: Number,
+    required: [true, "Provide your shop's pincode"],
+    minLength: [6, "6 Characters required."],
+    maxLength: [6, "6 Characters required."],
+  },
 });
