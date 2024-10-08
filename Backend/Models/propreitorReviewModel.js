08b02d6420af592adb7d6e6a311ccd1539bb2ec8
YYashSingh-9 <yyashsngh@gmail.com>
2024-10-08 06:18:25 -0700

    defining review schema

--- Backend/Models/propreitorReviewModel.js
+++ Backend/Models/propreitorReviewModel.js
@@ -17,4 +17,16 @@ const proprietorReviewSchema = mongoose.Schema({
     minLength: [6, "6 Characters required."],
     maxLength: [6, "6 Characters required."],
   },
+  title: {
+    type: String,
+    minLength: [5, "Minimum 5 characters required."],
+    required: [true, "Enter title."],
+    maxLength: [30, "Maxinmum 30 characters reached."],
+  },
+  reviewContent: {
+    type: String,
+    required: [true, "Please write your review."],
+    minLength: [30, "Minimum 30 characters are required."],
+    maxLength: [280, "Maxmimum 280 characters limit reached."],
+  },
 });
