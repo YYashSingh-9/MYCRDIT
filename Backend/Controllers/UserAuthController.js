900d32d0d1b3d3ebad0bb6aa1c9a5b49dfb52b03
YYashSingh-9 <yyashsngh@gmail.com>
2024-10-18 06:22:56 -0700

    more controllers

--- Backend/Controllers/UserAuthController.js
+++ Backend/Controllers/UserAuthController.js
@@ -1,3 +1,5 @@
 const Customer = require("./Models/customerModel");
 const Proprietor = require("./Models/proprietorModel");
 const jwt = require("jsonwebtoken");
+
+exports.customerAuthentication = () => {};
