ccbe17ba3a95489f8209464c956fd3ed9b4908ae
YYashSingh-9 <yyashsngh@gmail.com>
2024-10-27 09:06:06 -0700

    more

--- src/Components/UserUIComponents/NoteRequestPage.jsx
+++ src/Components/UserUIComponents/NoteRequestPage.jsx
@@ -9,6 +9,15 @@ const NoteRequestPage = () => {
       <BasicCoverDiv heading={"Your"} heading_highlight={" Pending requests."}>
         <Box className={classes.innerCover}>
           <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
+          <NoteRequestItem title={"Modi kirana parle g"} date={"14-5-25"} />
         </Box>
       </BasicCoverDiv>
     </>
