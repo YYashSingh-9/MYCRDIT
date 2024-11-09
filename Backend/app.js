92e4a16c77a51eda5d3f3c09eff6d0abd29079d4
YYashSingh-9 <yyashsngh@gmail.com>
2024-11-09 06:05:54 -0800

    adding middlewares

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const path = require("path");
+const xss = require("xss");
+const mongosanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+app.use(cookieParser());
+app.use(mongosanitize());
+app.use(xss());
+
+app.use((err, req, res, next) => {});
 module.exports = app;
