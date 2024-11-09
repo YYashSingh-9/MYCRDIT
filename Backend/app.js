1ddad8a17580455cca1de29b90968e9ca2697179
YYashSingh-9 <yyashsngh@gmail.com>
2024-11-09 06:10:48 -0800

    Security middlewares

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const xss = require("xss");
 const mongosanitize = require("express-mongo-sanitize");
 const cookieParser = require("cookie-parser");
+const rateLimiter = require("express-rate-limit");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,6 +12,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(mongosanitize());
 app.use(xss());
+
+const limiter = rateLimiter({
+  max: 100,
+  windowMs: 60 * 1000,
+  message:
+    "Request limits reached from this IP Address, try again after some time.",
+});
+
+app.use("/mycrdit/api", limiter);
 
 app.use((err, req, res, next) => {});
 module.exports = app;
