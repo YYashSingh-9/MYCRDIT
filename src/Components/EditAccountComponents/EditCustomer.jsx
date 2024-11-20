b5c3b55b4e4a85e2fd5cc90e6d9e5da7748e7e89
YYashSingh-9 <yyashsngh@gmail.com>
2024-11-20 05:50:01 -0800

    Edit account page

---
+++ src/Components/EditAccountComponents/EditCustomer.jsx
@@ -0,0 +1,54 @@
+import BasicCoverDiv from "../AdditionalComponents/BasicCoverDiv";
+import classes from "./EditCustomer.module.css";
+import { TextField, Box } from "@mui/material";
+import { Form } from "react-router-dom";
+import GeneralButton from "../AdditionalComponents/GeneralButton";
+
+const EditCustomer = () => {
+  return (
+    <>
+      <BasicCoverDiv heading={"Update your"} heading_highlight={" Information"}>
+        <Form method="POST" className={classes.form_main}>
+          <label htmlFor="Shop Name">Shop Name</label>
+          <br />
+
+          <TextField id="outlined" />
+          <hr />
+          <label htmlFor="Shop address">Shop Address</label>
+          <br />
+
+          <TextField id="outlined" />
+          <hr />
+          <label htmlFor="Shop Category">Shop Category</label>
+          <br />
+
+          <TextField id="outlined" />
+          <hr />
+          <label htmlFor="Contact Number">Contact Number</label>
+          <br />
+
+          <TextField id="outlined" />
+          <hr />
+          <label htmlFor="Proprietor's Name">Proprietor's Name</label>
+          <br />
+
+          <TextField id="outlined" />
+          <hr />
+          <label htmlFor="GST Number">GST Number</label>
+          <br />
+
+          <TextField id="outlined" />
+          <hr />
+          <Box className={classes.btnBox}>
+            <GeneralButton typeBtn="submit" btn_title="Save" />
+            <Link to={"/my-shop-info"}>
+              <GeneralButton typeBtn="button" btn_title="Back" />
+            </Link>
+          </Box>
+        </Form>
+      </BasicCoverDiv>
+    </>
+  );
+};
+
+export default EditCustomer;
