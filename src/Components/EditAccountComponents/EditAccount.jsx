897ae0010767e81e0ebfbf49447ba603e451ab08
YYashSingh-9 <yyashsngh@gmail.com>
2024-11-22 05:06:13 -0800

    DRY

--- src/Components/EditAccountComponents/EditAccount.jsx
+++ src/Components/EditAccountComponents/EditAccount.jsx
@@ -10,36 +10,18 @@ const EditCustomer = (props) => {
     <>
       <BasicCoverDiv heading={"Update your"} heading_highlight={" Information"}>
         <Form method="POST" className={classes.form_main}>
-          <label htmlFor="Shop Name">Shop Name</label>
-          <br />
+          {titleNHtmlFor.map((el, i) => {
+            return (
+              <>
+                <label htmlFor={el.titleNHtmlFor}>{el.titleNHtmlFor}</label>
+                <br />
 
-          <TextField id="outlined" />
-          <hr />
-          <label htmlFor="Shop address">Shop Address</label>
-          <br />
+                <TextField id="outlined" />
+                <hr />
+              </>
+            );
+          })}
 
-          <TextField id="outlined" />
-          <hr />
-          <label htmlFor="Shop Category">Shop Category</label>
-          <br />
-
-          <TextField id="outlined" />
-          <hr />
-          <label htmlFor="Contact Number">Contact Number</label>
-          <br />
-
-          <TextField id="outlined" />
-          <hr />
-          <label htmlFor="Proprietor's Name">Proprietor's Name</label>
-          <br />
-
-          <TextField id="outlined" />
-          <hr />
-          <label htmlFor="GST Number">GST Number</label>
-          <br />
-
-          <TextField id="outlined" />
-          <hr />
           <Box className={classes.btnBox}>
             <GeneralButton typeBtn="submit" btn_title="Save" />
             <Link to={"/my-shop-info"}>
@@ -48,19 +30,6 @@ const EditCustomer = (props) => {
           </Box>
         </Form>
       </BasicCoverDiv>
-      <Box>
-        {titleNHtmlFor.map((el, i) => {
-          return (
-            <>
-              <label htmlFor={el.titleNHtmlFor}>{el.titleNHtmlFor}</label>
-              <br />
-
-              <TextField id="outlined" />
-              <hr />
-            </>
-          );
-        })}
-      </Box>
     </>
   );
 };
