4ac07d3fcc8f532529b68f81dd5bc71176997ebc
YYashSingh-9 <yyashsngh@gmail.com>
2024-11-25 04:04:52 -0800

    mounting router

--- Backend/Routers/DebtNoteRouter.js
+++ Backend/Routers/DebtNoteRouter.js
@@ -1,2 +1,10 @@
 const express = require("express");
-const app = express.Router();
+const NoteRouter = express.Router();
+const NotesController = require("../Controllers/NotesController");
+const UserAuthController = require("../Controllers/UserAuthController");
+
+NoteRouter.route("/get-all-notes").get(
+  NotesController.allRunningNotes_proprietor
+);
+
+module.exports = NoteRouter;
