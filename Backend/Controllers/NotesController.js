6793906d80ae805d97e4cebe7673ba7d2f126ebd
YYashSingh-9 <yyashsngh@gmail.com>
2024-11-29 08:15:06 -0800

    more

--- Backend/Controllers/NotesController.js
+++ Backend/Controllers/NotesController.js
@@ -64,6 +64,11 @@ exports.paidNotePre_Controller = catchAsync(async (req, res, next) => {
   const date_Old = new Date(doc.date); // Date of note creation
   const date_New = new Date(paymentDate); // Date of payment
 
+  const date_old_ms = date_Old.getMilliseconds();
+  const date_New_ms = date_New.getMilliseconds();
+
+  console.log("First log -> Dates", date_Old, date_New);
+
   //3.
   const totalMilliseconds = date_New - date_Old;
   seconds = parseInt(Math.floor(totalMilliseconds / 1000));
@@ -71,8 +76,12 @@ exports.paidNotePre_Controller = catchAsync(async (req, res, next) => {
   hours = parseInt(Math.floor(minutes / 60));
   days = parseInt(Math.floor(hours / 24));
 
+  console.log("Log-> 2 timelines ", seconds, minutes, hours, days);
+
   const lengthOfPayment = days;
   const thirtyDayPayment = lengthOfPayment <= 30 ? true : false;
+
+  console.log("log-> 3", lengthOfPayment, thirtyDayPayment);
 
   const dummyObj = {
     debtNote_Id: debtNote_Id,
