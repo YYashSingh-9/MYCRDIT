8d149c167dab97c3809a29ab53aaceeddef030f9
YYashSingh-9 <yyashsngh@gmail.com>
2024-12-03 03:53:54 -0800

    accepting note middleware done

--- Backend/Routers/DebtNoteRouter.js
+++ Backend/Routers/DebtNoteRouter.js
@@ -3,6 +3,7 @@ const NoteRouter = express.Router();
 const NotesController = require("../Controllers/NotesController");
 const UserAuthController = require("../Controllers/UserAuthController");
 
+//PROPRIETOR NOTE ROUTES...
 NoteRouter.route("/get-all-notes").get(
   UserAuthController.protect,
   NotesController.allRunningNotes_proprietor
@@ -19,9 +20,10 @@ NoteRouter.route("/note-payment").post(
   NotesController.paidNotePre_Controller,
   NotesController.notePaidController
 );
-NoteRouter.route("/note-approval-request").patch(
-  UserAuthController.protect,
 
+//CUSTOMER NOTE ROUTES..
+NoteRouter.route("/note-approval-request").patch(
+  UserAuthController.customerProtectMiddleware,
   NotesController.acceptingNoteMiddleware
 );
 module.exports = NoteRouter;
