5ba175f18065093206ce6d73c1b5e50d274a69eb
YYashSingh-9 <yyashsngh@gmail.com>
2024-12-05 02:49:43 -0800

    more

--- Backend/Controllers/ProprietorReviewController.js
+++ Backend/Controllers/ProprietorReviewController.js
@@ -4,6 +4,8 @@ const catchAsync = require("../Utilities/catchAsync");
 exports.createReview = catchAsync(async (req, res, next) => {
   const { proprietorId } = req.body;
 
+  if (!proprietorId) return next(new Error("Proprietor's id is missing."));
+
   const doc = await proprietorReview.create(req.body);
 
   res.status(200).json({
