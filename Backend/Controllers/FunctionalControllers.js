dd2fa546e883f18b010fc0ac968f898a4ffdd520
YYashSingh-9 <yyashsngh@gmail.com>
2024-12-07 09:10:31 -0800

    Update shop info controller

---
+++ Backend/Controllers/FunctionalControllers.js
@@ -0,0 +1,4 @@
+const proprietor = require("../Models/proprietorModel");
+const catchAsync = require("../Utilities/catchAsync");
+
+exports.updateShopInfo = catchAsync(async (req, res, next) => {});
