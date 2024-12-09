90f36d9b393b65391d69c7cf1b5ea87d275a9c0d
YYashSingh-9 <yyashsngh@gmail.com>
2024-12-09 04:15:27 -0800

    more

--- Backend/Controllers/FunctionalControllers.js
+++ Backend/Controllers/FunctionalControllers.js
@@ -1,6 +1,23 @@
 const proprietor = require("../Models/proprietorModel");
 const catchAsync = require("../Utilities/catchAsync");
 
+const filterObj = (obj, ...allowedFields) => {
+  let newObject = {};
+  Object.keys(obj).forEach((curr_el) => {
+    if (allowedFields.includes(curr_el))
+      return (newObject[curr_el] = obj[curr_el]);
+  });
+};
+
 exports.updateShopInfo = catchAsync(async (req, res, next) => {
+  const fields = [
+    "ProprietorName",
+    "shopName",
+    "shopAddress",
+    "shopCategory",
+    "contactNumber",
+    "proprietorGST",
+  ];
+
   if (req.body.password) return next(new Error("invalid fields are included"));
 });
