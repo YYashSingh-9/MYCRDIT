bf35fc263d223db5492c01bc2bcd3bce2935045e
YYashSingh-9 <yyashsngh@gmail.com>
2024-12-12 08:03:01 -0800

    more error handling

--- Backend/Controllers/ErrorController.js
+++ Backend/Controllers/ErrorController.js
@@ -6,6 +6,23 @@ const validationError = (error) => {
   const message = Object.values(error.errors).map((el) => el.message);
   return new appError(message, 500);
 };
+const handleDuplicateError = (err) => {
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Got a duplicate field here ${value}`;
+  return new appError(message, 400);
+};
+const handleJWTError = () => {
+  const message = "Invalid token , try again later.";
+  return new appError(message, 400);
+};
+const handleJWTExpiredError = () => {
+  const message = "Your valdation token expired try loging in again..";
+  return new appError(message, 400);
+};
+const handleCastError = (error) => {
+  const message = `Invalid ${error.path}:${error.value}`;
+  return new appError(message, 400);
+};
 
 const developmentError = (err, req, res) => {
   console.log(err);
@@ -44,6 +61,13 @@ module.exports = (err, req, res, next) => {
   }
   if (process.env.NODE_ENV === "production") {
     let err_or = { ...error };
+    if (error.name === "CastError") err_or = handleCastError(err_or);
     if (error.name === "ValidationError") err_or = validationError(err_or);
+    if (error.code === 11000) err_or = handleDuplicateError(err_or);
+    if (error.name === "JsonWebTokenError") err_or = handleJWTError(err_or);
+    if (error.name === "TokenExpiredError")
+      err_or = handleJWTExpiredError(err_or);
+
+    productionError(err_or, req, res);
   }
 };
