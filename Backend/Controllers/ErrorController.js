8d1399954169173332485848515b9141acfadb4b
YYashSingh-9 <yyashsngh@gmail.com>
2024-12-12 05:32:51 -0800

    error handling

--- Backend/Controllers/ErrorController.js
+++ Backend/Controllers/ErrorController.js
@@ -3,9 +3,18 @@ const appError = require("../Utilities/appError");
 
 const productionError = (err, req, res) => {
   console.log(err);
+
+  res.status(err.statusCode).json({
+    status: "Fail",
+    data: err,
+    message: err.message,
+    errorName: err.name,
+  });
 };
 
 module.exports = (err, req, res, next) => {
   const error = JSON.parse(JSON.stringify(err));
+  error.message = error.message || "Failed request";
+  error.statusCode = error.statusCode || 500;
   productionError(error, req, res);
 };
