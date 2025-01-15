a40e0a490cfcc4b95bcc3e5d604f82debeb9e0c3
YYashSingh-9 <yyashsngh@gmail.com>
2025-01-15 05:44:06 -0800

    adding digital banner

--- src/Components/AdditionalComponents/DigitalBanner_forShop.jsx
+++ src/Components/AdditionalComponents/DigitalBanner_forShop.jsx
@@ -1,3 +1,17 @@
-const DigitalBanner = () => {};
+import { Box } from "@mui/material";
+import classes from "./DigitalBanner_forShop.module.css";
+
+const DigitalBanner = () => {
+  return (
+    <>
+      <Box className={classes.banner_box}>
+        <h3>Shop from</h3>
+        <h1>Modi Kirana !</h1>
+        <h4>Use MYCrdit account here to shop and relax..</h4>
+        <h4>find modi kirana at vidya nagar,near shiv mandir</h4>
+      </Box>
+    </>
+  );
+};
 
 export default DigitalBanner;
