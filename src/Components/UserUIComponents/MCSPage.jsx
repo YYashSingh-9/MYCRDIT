105d55dfb7fc29e8f1326cb5370431da64a0278d
YYashSingh-9 <yyashsngh@gmail.com>
2025-02-23 02:58:21 -0800

    mor

--- src/Components/UserUIComponents/MCSPage.jsx
+++ src/Components/UserUIComponents/MCSPage.jsx
@@ -17,22 +17,20 @@ const MCSPage = () => {
           marginBottom={3}
           className={classes.detailsBox}
         >
-          <h3>How MyCrdit score system works ?</h3>
+          <h3>How MyCrdit score system works ? {"(1)"}</h3>
           <p>
-            The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-            below for those interested. Sections 1.10.32 and 1.10.33 from "de
-            Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-            exact original form, accompanied by English versions from the 1914
-            translation by H. Rackham{" "}
+            Customer must clear his/her credit dues before 30 days of initiation
+            of debt note. On successful transaction of each debt note within 30
+            days, TCS will be added to customer's account. If customer somehow
+            pays after 40 days duration then no TCS
+            {"(Transactional credit score)"} will be added.
           </p>
           <br />
-          <h3>How MyCrdit score system works ?</h3>
+          <h3>How MyCrdit score system works ?{"(2)"}</h3>
           <p>
-            The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-            below for those interested. Sections 1.10.32 and 1.10.33 from "de
-            Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-            exact original form, accompanied by English versions from the 1914
-            translation by H. Rackham{" "}
+            Customer when consecutively clear his/her each debt{" "}
+            {"(within 30 days)"} for at least 3 months and then 6 months
+            continously, extra BCS {"(Behavioural credit score)"} will be added
           </p>{" "}
           <br />
           <h3>How MyCrdit score system works ?</h3>
