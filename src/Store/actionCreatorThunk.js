31a11c02a041c4373e1b0ce342afda159f5cd77c
YYashSingh-9 <yyashsngh@gmail.com>
2025-03-03 08:24:55 -0800

    more

--- src/Store/actionCreatorThunk.js
+++ src/Store/actionCreatorThunk.js
@@ -5,3 +5,24 @@ export const client = new QueryClient();
 // PROPRIETOR ACTION FUNCTIONS
 
 // 1. All running notes.
+
+export const getRequestFunction = async (urlType, cookie, additional) => {
+  let url = `http://localhost:3000/mycrdit/api/${urlType}`;
+
+  additional ? (url += `/${additional}`) : "";
+
+  const document = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${cookie}`,
+      "Content-type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      cookie: `jwt=${cookie}`,
+    },
+    redirect: "follow",
+  });
+  console.log(document);
+  const document_2 = await document.json();
+  return document_2;
+};
