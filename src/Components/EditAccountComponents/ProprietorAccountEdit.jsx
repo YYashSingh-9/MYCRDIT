d92022545854ea10d56b1f16c376728d01297b5e
YYashSingh-9 <yyashsngh@gmail.com>
2025-03-25 08:31:46 -0700

    more

--- src/Components/EditAccountComponents/ProprietorAccountEdit.jsx
+++ src/Components/EditAccountComponents/ProprietorAccountEdit.jsx
@@ -5,8 +5,8 @@ const ProprietorAccountEdit = () => {
   const titles = ["Proprietor Name", "Proprietor Number"];
   const userData = useSelector((state) => state.sliceOne.accountUserData);
   const acc_type = useSelector((state) => state.sliceOne.acc_type);
-  console.log(userData);
-  let proprietorData, backLink_Id;
+
+  let proprietorData, backLink_Id, isLoggedIn;
   if (userData && userData.status === "Success") {
     proprietorData = {
       name: userData.data.ProprietorName,
@@ -15,9 +15,14 @@ const ProprietorAccountEdit = () => {
 
     backLink_Id = `${acc_type},${userData.token}`;
   }
+  isLoggedIn = userData && userData.status === "Success" ? true : false;
   return (
     <>
-      <EditAccount titleNHtmlFor={titles} bck_btnLink={backLink_Id} />
+      <EditAccount
+        titleNHtmlFor={titles}
+        bck_btnLink={backLink_Id}
+        loginState={isLoggedIn}
+      />
     </>
   );
 };
