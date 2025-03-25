96d5fbac34a42148c9e9842d4b323fe167d525b2
YYashSingh-9 <yyashsngh@gmail.com>
2025-03-25 08:32:59 -0700

    more

--- src/Components/EditAccountComponents/CustomerAccountEdit.jsx
+++ src/Components/EditAccountComponents/CustomerAccountEdit.jsx
@@ -2,12 +2,25 @@ import EditAccount from "./EditAccount";
 
 const CustomerAccountEdit = () => {
   const titles = ["Customer Name", "Customer Number"];
+  const userData = useSelector((state) => state.sliceOne.accountUserData);
+  const acc_type = useSelector((state) => state.sliceOne.acc_type);
 
+  let customerData, backLink_Id, isLoggedIn;
+  if (userData && userData.status === "Success") {
+    customerData = {
+      name: userData.data.customerName,
+      contactNumber: userData.contactNumber,
+    };
+
+    backLink_Id = `${acc_type},${userData.token}`;
+  }
+  isLoggedIn = userData && userData.status === "Success" ? true : false;
   return (
     <>
       <EditAccount
         titleNHtmlFor={titles}
         bck_btnLink={"/your-account-details"}
+        loginState={isLoggedIn}
       />
     </>
   );
