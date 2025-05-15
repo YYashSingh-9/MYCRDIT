9afcecb8c7a3264155432cabb14b22b41d625c13
YYashSingh-9 <yyashsngh@gmail.com>
2025-05-15 07:33:51 -0700

    more

--- src/Components/AdditionalComponents/FormMain.jsx
+++ src/Components/AdditionalComponents/FormMain.jsx
@@ -7,7 +7,14 @@ import {
 } from "@mui/material";
 import classes from "./FormMain.module.css";
 import FiberNewIcon from "@mui/icons-material/FiberNew";
-import { Form, Link, useActionData, useNavigate } from "react-router-dom";
+import Spinner from "./Spinner";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 import GeneralButton from "./GeneralButton";
 import InitialSlider from "./InitialSlider";
 import { useSelector } from "react-redux";
@@ -18,6 +25,7 @@ const FormMain = () => {
   const data = useSelector((state) => state.sliceOne.accountUserData);
   const formData = useActionData();
   const navigate = useNavigate();
+  const navigation = useNavigation();
   if (data.status === "Success") {
     cookie = data.token;
     proprietorId = data.data._id;
@@ -32,77 +40,88 @@ const FormMain = () => {
   return (
     <>
       <InitialSlider />
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        className={classes.mainGrid}
-      >
-        <Grid item xs={12} sm={12} md={12} lg={12} className={classes.header}>
-          <Box className={classes.title}>
-            <FiberNewIcon className={classes.icn} />
-            <h3>New Debt Note</h3>
-          </Box>
+      {navigation.state === "submitting" ? (
+        <Spinner />
+      ) : (
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          className={classes.mainGrid}
+        >
+          <Grid item xs={12} sm={12} md={12} lg={12} className={classes.header}>
+            <Box className={classes.title}>
+              <FiberNewIcon className={classes.icn} />
+              <h3>New Debt Note</h3>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            className={classes.formDiv}
+          >
+            <Form method="POST" className={classes.form_main}>
+              <label htmlFor="Note Title">Note title</label>
+              <br />
+
+              <TextField id="outlined" name="noteTitle" />
+              <hr />
+              <label htmlFor="Customer Name">Customer Name</label>
+              <br />
+
+              <TextField id="outlined" name="customerName" />
+              <hr />
+              <label htmlFor="Customer Number">Customer Number</label>
+              <br />
+
+              <TextField id="outlined" name="customerNumber" />
+              <hr />
+
+              <label htmlFor="Product bought on credit">
+                Product bought on credit :-
+              </label>
+              <br />
+
+              <label htmlFor="Product bought on credit">Brand of product</label>
+              <br />
+              <TextField id="outlined" name="productBrand" />
+              <hr />
+              <label htmlFor="Product bought on credit">Product name</label>
+              <br />
+              <TextField id="outlined" name="productName" />
+              <hr />
+              <label htmlFor="Amount payable">Amount</label>
+              <br />
+              <OutlinedInput
+                id="Amount payable"
+                name="amount"
+                startAdornment={
+                  <InputAdornment position="start">₹</InputAdornment>
+                }
+                label="Amount"
+              />
+              <hr />
+              <label htmlFor="Due date">Date</label>
+              <br />
+              <input type="date" id="dueDate" name="date" />
+              <hr />
+              <GeneralButton
+                typeBtn="submit"
+                btn_title="Save"
+                name="proprietor-data"
+                value={btnDataToSend}
+              />
+              <Link to={".."}>
+                <GeneralButton btn_title="Back" />
+              </Link>
+            </Form>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={12} md={12} lg={12} className={classes.formDiv}>
-          <Form method="POST" className={classes.form_main}>
-            <label htmlFor="Note Title">Note title</label>
-            <br />
-
-            <TextField id="outlined" name="noteTitle" />
-            <hr />
-            <label htmlFor="Customer Name">Customer Name</label>
-            <br />
-
-            <TextField id="outlined" name="customerName" />
-            <hr />
-            <label htmlFor="Customer Number">Customer Number</label>
-            <br />
-
-            <TextField id="outlined" name="customerNumber" />
-            <hr />
-
-            <label htmlFor="Product bought on credit">
-              Product bought on credit :-
-            </label>
-            <br />
-
-            <label htmlFor="Product bought on credit">Brand of product</label>
-            <br />
-            <TextField id="outlined" name="productBrand" />
-            <hr />
-            <label htmlFor="Product bought on credit">Product name</label>
-            <br />
-            <TextField id="outlined" name="productName" />
-            <hr />
-            <label htmlFor="Amount payable">Amount</label>
-            <br />
-            <OutlinedInput
-              id="Amount payable"
-              name="amount"
-              startAdornment={
-                <InputAdornment position="start">₹</InputAdornment>
-              }
-              label="Amount"
-            />
-            <hr />
-            <label htmlFor="Due date">Date</label>
-            <br />
-            <input type="date" id="dueDate" name="date" />
-            <hr />
-            <GeneralButton
-              typeBtn="submit"
-              btn_title="Save"
-              name="proprietor-data"
-              value={btnDataToSend}
-            />
-            <Link to={".."}>
-              <GeneralButton btn_title="Back" />
-            </Link>
-          </Form>
-        </Grid>
-      </Grid>
+      )}
     </>
   );
 };
--- src/Components/TopLevel/DebtDetailsPage.jsx
+++ src/Components/TopLevel/DebtDetailsPage.jsx
@@ -18,6 +18,7 @@ import DetailedNote from "../AdditionalComponents/DetailDebtNote";
 import EditIcon from "@mui/icons-material/Edit";
 import SpeakerNotesOffIcon from "@mui/icons-material/SpeakerNotesOff";
 import GeneralButton from "../AdditionalComponents/GeneralButton";
+import Spinner from "../AdditionalComponents/Spinner";
 
 const DebtDetailsPage = () => {
   let isFormActive = false;
@@ -57,7 +58,7 @@ const DebtDetailsPage = () => {
   });
 
   //2. Getting all the notes of same customer number
-  const { mutate, data, isLoading } = useMutation({
+  const { mutate, data, isPending } = useMutation({
     mutationKey: ["note-details"],
     mutationFn: () => {
       return getAllCustomerNotes(customerNumber, cookie);
@@ -84,7 +85,7 @@ const DebtDetailsPage = () => {
       cookie: cookie,
       type: funcIntent,
     };
-    console.log(obj);
+
     patch_requestHandle(obj);
   };
 
@@ -108,95 +109,101 @@ const DebtDetailsPage = () => {
             alignItems="center"
             className={classes.main}
           >
-            <Grid
-              item
-              lg={12}
-              sm={12}
-              md={12}
-              xs={12}
-              className={classes.headBar}
-            >
-              <header className={classes.title}>
-                <h3>Debt Notes of {customerName}</h3>
-              </header>
-            </Grid>
-            <Grid
-              item
-              lg={12}
-              sm={12}
-              md={12}
-              xs={12}
-              className={classes.addBtn}
-            >
-              <Link to={`/${id}/details/add-note`}>
-                <button>
-                  <EditIcon className={classes.iccn} />
-                  Add
-                </button>
-              </Link>
-              <Link to={".."}>
-                <button>
-                  <ArrowLeft className={classes.iccn} />
-                  Back
-                </button>
-              </Link>
-            </Grid>
-            <hr
-              style={{
-                width: "100%",
-                height: "0.1rem ",
-                backgroundColor: "rgb(211, 211, 211 )",
-                borderRadius: "1rem",
-                outline: "none",
-                border: "none",
-                marginTop: "0rem",
-                marginBottom: "1rem",
-              }}
-            />
-            <Grid item lg={12} sm={12} md={12} xs={12} width={"100%"}>
-              <Box className={classes.infoNote}>
-                <p>
-                  Info : If customer hasn't accepted their debt note then paid
-                  option will not be allowed.
-                </p>
-                <br />
-                <p>
-                  Paid option will also be not available till 20 days after
-                  accepting the note is completed.
-                </p>
-              </Box>
-            </Grid>
-            <Grid
-              item
-              lg={12}
-              sm={12}
-              md={12}
-              xs={12}
-              className={classes.parentContainer}
-            >
-              {arrayOfNotes.length >= 1 ? (
-                arrayOfNotes.map((el, i) => {
-                  return (
-                    <DetailedNote
-                      data={el}
-                      key={i}
-                      cookie={cookie}
-                      acc={accType}
-                      patch_func={patchHandler}
-                    />
-                  );
-                })
-              ) : (
-                <Box className={classes.dummyBox}>
-                  {" "}
-                  <h3>No debt notes to display </h3>{" "}
-                  <SpeakerNotesOffIcon className={classes.dummyIcn} />
-                  <Link to={".."}>
-                    <GeneralButton btn_title="Home" />
+            {isPending === true && <Spinner />}
+            {data && data.status === "Success" && (
+              <>
+                {" "}
+                <Grid
+                  item
+                  lg={12}
+                  sm={12}
+                  md={12}
+                  xs={12}
+                  className={classes.headBar}
+                >
+                  <header className={classes.title}>
+                    <h3>Debt Notes of {customerName}</h3>
+                  </header>
+                </Grid>
+                <Grid
+                  item
+                  lg={12}
+                  sm={12}
+                  md={12}
+                  xs={12}
+                  className={classes.addBtn}
+                >
+                  <Link to={`/${id}/details/add-note`}>
+                    <button>
+                      <EditIcon className={classes.iccn} />
+                      Add
+                    </button>
                   </Link>
-                </Box>
-              )}
-            </Grid>
+                  <Link to={".."}>
+                    <button>
+                      <ArrowLeft className={classes.iccn} />
+                      Back
+                    </button>
+                  </Link>
+                </Grid>
+                <hr
+                  style={{
+                    width: "100%",
+                    height: "0.1rem ",
+                    backgroundColor: "rgb(211, 211, 211 )",
+                    borderRadius: "1rem",
+                    outline: "none",
+                    border: "none",
+                    marginTop: "0rem",
+                    marginBottom: "1rem",
+                  }}
+                />
+                <Grid item lg={12} sm={12} md={12} xs={12} width={"100%"}>
+                  <Box className={classes.infoNote}>
+                    <p>
+                      Info : If customer hasn't accepted their debt note then
+                      paid option will not be allowed.
+                    </p>
+                    <br />
+                    <p>
+                      Paid option will also be not available till 20 days after
+                      accepting the note is completed.
+                    </p>
+                  </Box>
+                </Grid>
+                <Grid
+                  item
+                  lg={12}
+                  sm={12}
+                  md={12}
+                  xs={12}
+                  className={classes.parentContainer}
+                >
+                  {arrayOfNotes.length >= 1 ? (
+                    arrayOfNotes.map((el, i) => {
+                      return (
+                        <DetailedNote
+                          data={el}
+                          key={i}
+                          cookie={cookie}
+                          acc={accType}
+                          patch_func={patchHandler}
+                        />
+                      );
+                    })
+                  ) : (
+                    <Box className={classes.dummyBox}>
+                      {" "}
+                      <h3>No debt notes to display </h3>{" "}
+                      <SpeakerNotesOffIcon className={classes.dummyIcn} />
+                      <Link to={".."}>
+                        <GeneralButton btn_title="Home" />
+                      </Link>
+                    </Box>
+                  )}
+                </Grid>{" "}
+              </>
+            )}
           </Grid>
         </>
       )}
